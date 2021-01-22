bf42827dc21d4b27436761b3e4666e051ff90a72
Robin Scholtes <robin@theweirdscience.com>
2021-01-22 21:34:36 +1300

    an extra plea status

--- src/model/plea/plea-status.enum.ts
+++ src/model/plea/plea-status.enum.ts
@@ -3,9 +3,10 @@ export enum PLEA_STATUS {
   NOTIFIED = "Notified",
   RESPONDED = "Responded",
   AWAITING = "Awaiting",
-  COMPLIED = "Complied",
+  COMPLIED = "Complied"
 }
 
 // note: AWAITING is in stage between responded and complied,
 // where a company may have pledged a vegan product but we are
 // waiting for actual launch of promised product
+
