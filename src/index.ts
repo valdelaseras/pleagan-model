a87cc46ea214848ceddac8b42a824f9002737629
Timothy Claassens <tim.claassens@gmail.com>
2021-02-21 02:37:44 +1300

    v0.0.31

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { IPlea, PLEA_STATUS, PLEA_TARGET } from "./model/plea";
 import { IProduct } from "./model/product";
-import { IPleagan, IUserSettings, INotificationSettings } from "./model/pleagan";
+import {
+  IPleagan,
+  IUserSettings,
+  INotificationSettings,
+} from "./model/pleagan";
 import { ICompany } from "./model/company";
 
-export { IPlea, IPleagan, IUserSettings, INotificationSettings, IProduct, ICompany, PLEA_STATUS, PLEA_TARGET };
+export {
+  IPlea,
+  IPleagan,
+  IUserSettings,
+  INotificationSettings,
+  IProduct,
+  ICompany,
+  PLEA_STATUS,
+  PLEA_TARGET,
+};
--- src/model/product/base/product.interface.ts
+++ src/model/product/base/product.interface.ts
@@ -1,7 +1,7 @@
 export interface IProduct {
   id?: number;
   name: string;
-  vegan?: boolean;
+  vegan: boolean;
   imageUrl?: string;
   animalIngredients?: string[];
 }
