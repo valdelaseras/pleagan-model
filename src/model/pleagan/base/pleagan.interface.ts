5861bceaf45de45cb03af5482e4e6d988d4cd429
Timothy Claassens <tim.claassens@gmail.com>
2021-02-21 20:24:13 +1300

    v0.0.37

--- src/model/pleagan/base/pleagan.interface.ts
+++ src/model/pleagan/base/pleagan.interface.ts
@@ -1,4 +1,6 @@
-import { IUserSettings } from "../settings/user-settings.interface";
+import { IUserSettings } from "../settings";
+import { ISupport } from "../../plea/base/support.interfase";
+import { IPlea } from "../../../index";
 
 export interface IPleagan {
   uid: string;
@@ -8,4 +10,6 @@ export interface IPleagan {
   displayName?: string;
   country?: string;
   settings?: IUserSettings;
+  supports?: ISupport[];
+  initiatedPleas?: IPlea[];
 }
