dd9a0fef45575cd4e816b536a84529ab5960551b
Timothy Claassens <tim.claassens@gmail.com>
2021-03-03 16:33:39 +1300

    v0.0.46

--- src/model/plea/base/plea.interface.ts
+++ src/model/plea/base/plea.interface.ts
@@ -2,14 +2,15 @@ import { ICompany } from "../../company";
 import { PLEA_STATUS } from "../enum";
 import { IProduct } from "../../product";
 import { ISupport } from "./support.interfase";
+import { IPleagan } from "../../../index";
 
 export interface IPlea {
   description: string;
   status: PLEA_STATUS;
   company: ICompany;
   nonVeganProduct: IProduct;
-  initiatorUid?: string;
   numberOfSupports: number;
+  pleagan?: IPleagan;
   id?: number;
   createdAt?: Date;
   updatedAt?: Date;
