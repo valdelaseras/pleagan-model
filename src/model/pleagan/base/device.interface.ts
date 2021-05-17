3011a1e3a04dee08d3322991c732f21050718a64
Timothy Claassens <tim.claassens@gmail.com>
2021-05-17 22:56:01 +1200

    v0.0.65

--- src/model/pleagan/base/device.interface.ts
+++ src/model/pleagan/base/device.interface.ts
@@ -1,7 +1,7 @@
-import { INotificationSettings } from "../settings/notifications/notification-settings.interface";
-
 export interface IDevice {
   token: string;
   uuid: string;
-  settings: INotificationSettings;
+  notifyOnMyPleas: boolean;
+  notifyOnSupportedPleas: boolean;
+  notifyOnNews: boolean;
 }
